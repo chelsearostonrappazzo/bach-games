c9287b42987163bcbbbe4602eaeef975f9b1ca55
Chelsea Roston <cmroston@gmail.com>
2021-04-21 20:54:39 -0400

    working copy

--- game.rb
+++ game.rb
@@ -2,87 +2,83 @@
 require "tty-prompt"
 @prompt = TTY::Prompt.new
 
-file = "male_celebs.txt"
+# file = "male_celebs.txt"
+# file_two = "sara.txt"
 # 20 total names
 @all_names = []
-celeb_names = []
-filler_names = []
-entered_names = []
+@celeb_names = []
+@filler_names = []
+@mundane_names = []
 @tops = []
 @bottoms = []
-@ind = 0
-@var = 1
+@ind, @var = 0, 1
 
-# this will pair the names up with no repeats
-def pairings(nam)
-  puts "Your contestants are:"
-  while @var < nam.length
-    puts "#{@var}. #{nam[@ind]}"
-    @var += 1
-    @ind += 1
-  end
-  puts "--------"
-  pairings = nam.combination(2).to_a
-  @game = pairings.shuffle
-end
+# File.readlines(file).each do |line|
+#   @celeb_names << line.strip
+# end
 
-# this will pull celeb names from file and put into an array
-File.readlines(file).each do |line|
-  celeb_names << line.strip
-end
+# File.readlines(file_two).each do |line|
+#   @mundane_names << line.strip
+# end
 
-# this will populate a new array of names based on user input.
-puts "Please enter in names to add to the list. Type 'done' when you are finished!"
-input = nil
-while input == gets.chomp.capitalize
-  entered_names << input
-  break if input.downcase == "done"
-end
+# def pairings(nam)
+#   puts "Your contestants are:"
+#   while @var < nam.length
+#     puts "#{@var}. #{nam[@ind]}"
+#     @var += 1
+#     @ind += 1
+#   end
+#   puts "--------"
+#   pairings = nam.combination(2).to_a
+#   @game = pairings.shuffle
+# end
 
-# this will check if there are less than 20 names, if so, it will fill with names from a list of celebrities.
-len = entered_names.length
-if len < 20
-  filler_names << celeb_names.sample(20 - len)
-  @all_names = entered_names.concat(filler_names).flatten
-  pairings(@all_names)
-elsif len > 20
-  entered_names.delete_if { |num| entered_names.index(num) > 19 }
-  pairings(entered_names)
-end
+# def get_contestants
+#   if @mundane_names.length < 20
+#     @filler_names << @celeb_names.sample(20 - len)
+#     @all_names = @mundane_names.concat(@filler_names).flatten
+#     pairings(@all_names)
+#   elsif @mundane_names.length > 20
+#     @mundane_names.delete_if { |num| @mundane_names.index(num) > 19 }
+#     pairings(@mundane_names)
+#   end
+# end
 
-def check_ranking
-  @tops_rankings = Hash.new(0)
-  @bottoms_rankings = Hash.new(0)
-  @tops.each { |name| @tops_rankings[name] += 1 }
-  @bottoms.each { |name| @bottoms_rankings[name] += 1 }
-  p @tops_rankings
-  p @bottoms_rankings
-end
+# def start_game
+#   start = @prompt.select("Are you ready to begin?", %w(Yes No))
+#   @ind = 0
+#   while start == "Yes"
+#     puts "1. #{@game[@ind][0]}" "\n" "2. #{@game[@ind][1]}"
+#     puts "*~*~*~*~* \n Who is the top?"
+#     choice = gets.chomp.to_i
+#     topbot(choice)
+#     @ind += 1
+#   end
+# end
 
-def start_game
-  start = @prompt.select('Are you ready to begin?', %w(Yes No))
-  @ind = 0
-  while start == "Yes"
-    puts "1. #{@game[@ind][0]}" "\n" "2. #{@game[@ind][1]}"
-    puts "*~*~*~*~* \n Who is the top?"
-    choice = gets.chomp.to_i
-    topbot(choice)
-    @ind += 1
-    check_ranking
-    end
-end
+# def check_ranking
+#   results = File.open("results.txt", "w+")
+#   @rankings = Hash.new { |h, k| h[k] = Hash.new(&h.default_proc) }
+#   @all_names.each { |name| @rankings[:tops][name] = 0 }
+#   @all_names.each { |name| @rankings[:bottoms][name] = 0 }
+#   @tops.each { |name| @rankings[name] += 1 }
+#   @bottoms.each { |name| @rankings[name] += 1 }
+#   results << @rankings.sort_by { |name, num| num }.to_h
+# end
 
-def topbot(choice)
-  return unless choice.positive? && choice < 3
+# def topbot(choice)
+#   return unless choice.positive? && choice < 3
 
-  top_index = (choice - 1).zero? ? 0 : 1
-  bottom_index = (choice - 1).zero? ? 1 : 0
-  @tops << @game[@ind][top_index]
-  @bottoms << @game[@ind][bottom_index]
-end
+#   top_index = (choice - 1).zero? ? 0 : 1
+#   bottom_index = (choice - 1).zero? ? 1 : 0
+#   @tops << @game[@ind][top_index]
+#   @bottoms << @game[@ind][bottom_index]
+# end
 
-def rankings
-  
+def save
+  output = File.open("bach_game.txt", "w")
+  output << rankings
+  output.close
 end
 
 start_game
--- main.rb
+++ main.rb
@@ -2,15 +2,24 @@ require_relative "idols.rb"
 require_relative "idol_data.rb"
 # require_relative "tropes.rb"
 # require_relative "game.rb"
+require "FileUtils"
+require_relative "tropes.txt"
+require_relative "male_celebs.txt"
 
-new_one = Group.new(group_name: "Everglow")
-new_two = Group.new(group_name: "Stray Kids")
-new_three = Group.new(group_name: "Way V")
+if FileUtils.compare_file("tropes.txt", "male_celebs.txt")
+  true
+else
+  false
+end
 
-new_one.add_idols
-new_two.add_idols
-new_three.add_idols
+# new_one = Group.new(group_name: "Everglow")
+# new_two = Group.new(group_name: "Stray Kids")
+# new_three = Group.new(group_name: "Way V")
 
-game = Idol_Database.new(groups: [new_one, new_two, new_three])
+# new_one.add_idols
+# new_two.add_idols
+# new_three.add_idols
 
-p game.groups
+# game = Idol_Database.new(groups: [new_one, new_two, new_three])
+
+# p game.groups
